3dca522bdd21c458821a86a4e8fa592c0dccd1f5
r-umemoto <umemoto.biz.g@gmail.com>
2026-04-05 12:04:49 +0900

    オートローダーとリストを追加

---
+++ student_list.js
@@ -0,0 +1,5 @@
+// 自動生成されたファイルです。手動で編集しないでください。
+const studentFiles = [
+  'ai_template.js',
+  'ai_test.js',
+];
---
+++ students/ai_test.js
@@ -0,0 +1,6 @@
+GhostRegistry["テスト用"] = {
+    color: "purple",
+    logic: function (myX, myY, pX, pY, dirs) {
+        return dirs[0]; // 単純に最初の方向へ
+    }
+};
